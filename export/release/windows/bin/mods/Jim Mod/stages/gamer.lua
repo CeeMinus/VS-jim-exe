791b8e75437858b5d024bc47771f7693006c3daf
Chase-333 <chaseraymondwimpy@gmail.com>
2021-11-23 16:20:24 -0500

    All of the exe files (:

---
+++ export/release/windows/bin/mods/Jim Mod/stages/gamer.lua
@@ -0,0 +1,19 @@
+function onCreate()
+	
+	makeLuaSprite('gamercrib','gamer_cave',-300,-100)
+	addLuaSprite('gamercrib',false)
+
+	close(true); --For performance reasons
+end
+
+function onBeatHit()
+	
+end
+
+function onStepHit()
+	
+end
+
+function onUpdate()
+	
+end
