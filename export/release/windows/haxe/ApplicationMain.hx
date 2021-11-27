4455e66b56bd3f328e567d4fc83b3ddeeaec6306
Chase-333 <chaseraymondwimpy@gmail.com>
2021-11-26 22:49:26 -0500

    zamn zaniel

--- export/release/windows/haxe/ApplicationMain.hx
+++ export/release/windows/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "42";
+		app.meta["build"] = "43";
 		app.meta["company"] = "ShadowMario";
 		app.meta["file"] = "PsychEngine";
 		app.meta["name"] = "Friday Night Funkin': Psych Engine";
--- export/release/windows/obj/src/__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf.cpp
+++ export/release/windows/obj/src/__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf.cpp
@@ -14,12 +14,12 @@
 #include <openfl/text/Font.h>
 #endif
 
-HX_DEFINE_STACK_FRAME(_hx_pos_78712a569baebf83_995_new,"__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf","new",0xcba1a9da,"__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf.new","ManifestResources.hx",995,0xf77aa668)
+HX_DEFINE_STACK_FRAME(_hx_pos_78712a569baebf83_997_new,"__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf","new",0xcba1a9da,"__ASSET__OPENFL__flixel_fonts_nokiafc22_ttf.new","ManifestResources.hx",997,0xf77aa668)
 
 void __ASSET__OPENFL__flixel_fonts_nokiafc22_ttf_obj::__construct(){
-            	HX_GC_STACKFRAME(&_hx_pos_78712a569baebf83_995_new)
-HXDLIN( 995)		this->_hx___fromLimeFont( ::__ASSET__flixel_fonts_nokiafc22_ttf_obj::__alloc( HX_CTX ));
-HXDLIN( 995)		super::__construct(null());
+            	HX_GC_STACKFRAME(&_hx_pos_78712a569baebf83_997_new)
+HXDLIN( 997)		this->_hx___fromLimeFont( ::__ASSET__flixel_fonts_nokiafc22_ttf_obj::__alloc( HX_CTX ));
+HXDLIN( 997)		super::__construct(null());
             	}
 
 Dynamic __ASSET__OPENFL__flixel_fonts_nokiafc22_ttf_obj::__CreateEmpty() { return new __ASSET__OPENFL__flixel_fonts_nokiafc22_ttf_obj; }
